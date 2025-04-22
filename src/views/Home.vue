3adee74cdca77d1f7807ebb29cf50f4f96eb2a8e
younggg1 <1064254619@qq.com>
2025-04-22 19:28:06 +0800

    初始化项目

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,11 @@
 <template>
   <div>
     <h1>LeetCodeTrack</h1>
-    <SunMoon />
+    
   </div>
 </template>
 
 <script setup>
-import SunMoon from '@/components/SunMoon.vue'
 </script>
 
 <style scoped>
